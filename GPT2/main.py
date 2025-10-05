31a28e2cc6aa292e8c78fd897e55eed146d9705c
Jonah Cha <jonahc426@gmail.com>
2025-10-04 18:59:41 -0700

    weight initialization

--- GPT2/main.py
+++ GPT2/main.py
@@ -13,6 +13,7 @@ class CausalSelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
@@ -81,6 +82,21 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
 
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= 2 * self.config.n_layer ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot foward sequence of length {T}..."
@@ -147,7 +163,6 @@ class GPT(nn.Module):
 
         return model
     
-#-------------------------------------------------------------------------------------------
 import tiktoken
 
 class DataLoaderLite:
@@ -175,6 +190,11 @@ class DataLoaderLite:
             self.current_position = 0
         return x, y
     
+#-------------------------------------------------------------------------------------------
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
 
 train_loader = DataLoaderLite(B=4, T=32)
 
