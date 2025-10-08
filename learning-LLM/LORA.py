04df838fbf158e2f485ebc6829a1221271dd4fc9
Jonah Cha <jonahc426@gmail.com>
2025-10-07 21:36:07 -0700

    TODO: responds to natural language feedback

--- learning-LLM/LORA.py
+++ learning-LLM/LORA.py
@@ -47,7 +47,7 @@ def chat(model, tokenizer):
     while True:
         user_input = input("User: ")
         inputs = tokenizer(user_input, return_tensors="pt").to(model.device)
-        outputs = model.generate(**inputs, max_new_tokens=50)
+        outputs = model.generate(**inputs, max_new_tokens=200)
         response = tokenizer.decode(outputs[0], skip_special_tokens=True)
         print("Bot:", response)
         feedback = input("Rate response (-1, 0, 1): ")
